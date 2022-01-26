611d0e55098a591f80002951b1b2c672180b96bd
Dmitry A. Anufriev <dmitryanufriev@gmail.com>
2022-01-26 22:46:02 +0500

    Add fzf support to Telescope

--- init.lua
+++ init.lua
@@ -27,13 +27,30 @@ require('packer').startup(function()
   }
 
   -- Telescope
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim', 
+    run = 'make' 
+  }
+
   use { 
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} },
     config = function() 
-      require'telescope'.setup {} 
+      require'telescope'.setup {
+        extensions = {
+          fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                             -- the default case_mode is "smart_case"
+          }
+        }
+      } 
     end, 
   }
+
+  require('telescope').load_extension('fzf')
 
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   use {
@@ -49,10 +66,8 @@ require('packer').startup(function()
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
-  use 'saadparwaiz1/cmp_luasnip'
-
-  --- Автодополнлялка к файловой системе
   use 'hrsh7th/cmp-path'
+  use 'saadparwaiz1/cmp_luasnip'
 
   -- Show LSP signature info (https://github.com/ray-x/lsp_signature.nvim)
   use 'ray-x/lsp_signature.nvim'
